0a6536d2c5661bb2cac40b873ee922ba4a718879
unknown <sr0589071@gmail.com>
2024-09-24 11:25:17 +0600

    added an navbar section

---
+++ js/donate.js
@@ -0,0 +1,5 @@
+// donation and history section
+document.getElementById("history-button").addEventListener("click",function(){
+    document.getElementById("donationSection").classList.add("hidden");
+    document.getElementById("historySection").classList.add("hidden");
+})
