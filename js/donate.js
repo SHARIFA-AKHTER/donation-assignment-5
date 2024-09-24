468634cc60104ced5134d630f725b084dbe27409
unknown <sr0589071@gmail.com>
2024-09-24 20:30:58 +0600

    create navbar item file

--- js/donate.js
+++ js/donate.js
@@ -1,5 +1,5 @@
-// donation and history section
-document.getElementById("history-button").addEventListener("click",function(){
-    document.getElementById("donationSection").classList.add("hidden");
-    document.getElementById("historySection").classList.add("hidden");
-})
+
+// function showAlert(){
+//     alert("Thank you for your donation!")
+// }
+
